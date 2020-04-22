fe244ee21909bbf699e0f5967c6f57af0dd55c75
Slayerbfg <43501324+Hailrakebfg@users.noreply.github.com>
2020-04-22 16:05:08 +0300

    Add files via upload

---
+++ script3fail.bat
@@ -0,0 +1,14 @@
+echo off
+rem go to into firstcat
+cd firstcat
+rem create firstfile
+echo > first.txt
+rem go to into secondcat
+cd secondcat
+rem create secondfile
+echo > secondfile.txt
+rem go to into secondcat
+cd thirdcat
+rem create thitdfile
+echo > thirdfile.txt
+pause
